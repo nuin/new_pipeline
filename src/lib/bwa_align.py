9c15b61316f2073cd2ad1a07c5143fc7c8657246
nuin <nuin@genedrift.org>
2023-09-21 14:42:33 -0600

    aligning files

--- src/lib/bwa_align.py
+++ src/lib/bwa_align.py
@@ -9,6 +9,10 @@ import os
 import subprocess
 import logging
 
+from rich.console import Console
+
+console = Console()
+
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
@@ -26,38 +30,60 @@ def run_bwa(sample_id, fastq_files, datadir, reference, bwa, samtools):
     :return:
     """
 
-    logger.info('Starting bwa processing for file ' + sample_id)
+    console.log(f"Starting bwa processing for file {sample_id}")
 
     bam_index_check = 0
-    if os.path.isfile(datadir + "/BAM/" + sample_id + "/" + sample_id + ".bam") or os.path.isfile(datadir + "/BAM/" + sample_id + "/BAM/" + sample_id + ".bam"):
-        logger.info(sample_id + ' BAM file exists')
+    if os.path.isfile(
+        datadir + "/BAM/" + sample_id + "/" + sample_id + ".bam"
+    ) or os.path.isfile(datadir + "/BAM/" + sample_id + "/BAM/" + sample_id + ".bam"):
+        console.log(f"{sample_id} BAM file exists")
         bam_index_check += 1
-    elif os.path.isfile(datadir + "/BAM/" + sample_id + "/" + sample_id + ".bam.gz") or os.path.isfile(datadir + "/BAM/" + sample_id + "/BAM/" + sample_id + ".bam"):
-        logger.info(sample_id + ' BAM file exists and compressed')
+    elif os.path.isfile(
+        datadir + "/BAM/" + sample_id + "/" + sample_id + ".bam.gz"
+    ) or os.path.isfile(datadir + "/BAM/" + sample_id + "/BAM/" + sample_id + ".bam"):
+        console.log(f"{sample_id} BAM file exists and compressed")
         bam_index_check += 1
     else:
         bwa_string = "%s mem -t 16 %s %s " % (bwa, reference, " ".join(fastq_files))
-        bwa_string += "| %s view -Sb - | %s sort - -o %s" % (samtools, samtools, datadir + "/BAM/" + sample_id + "/BAM/" + sample_id + ".bam")
-        bwa_string += " && %s index %s" % (samtools, datadir + "/BAM/" + sample_id + "/BAM/" + sample_id + ".bam")
-        logger.info('Command ' + bwa_string)
-        proc = subprocess.Popen(bwa_string, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        bwa_string += "| %s view -Sb - | %s sort - -o %s" % (
+            samtools,
+            samtools,
+            datadir + "/BAM/" + sample_id + "/BAM/" + sample_id + ".bam",
+        )
+        bwa_string += " && %s index %s" % (
+            samtools,
+            datadir + "/BAM/" + sample_id + "/BAM/" + sample_id + ".bam",
+        )
+        console.log(f"{bwa_string}")
+        proc = subprocess.Popen(
+            bwa_string, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+        )
         while True:
             output = proc.stderr.readline().strip()
             if output == b"":
                 break
             else:
-                logger.info(output.decode("utf-8"))
+                console.log(output.decode("utf-8"))
         # proc.wait()
         bam_index_check = 0
-        logger.info('BWA alignment completed for sample ' + sample_id)
+        console.log(f"{sample_id} BAM file created")
 
     return bam_index_check
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
-    data_datadir = '/Users/nuin/Projects/Data/Test_dataset'
-    sample_id = 'NA12877_1'
-    reference = '/opt/reference/hg19.fasta'
-    run_bwa(sample_id, ['/Users/nuin/Projects/Data/Test_dataset/BaseCalls/NA12877_1_S1_L001_R1_001.fastq.gz',
-                         '/Users/nuin/Projects/Data/Test_dataset/BaseCalls/NA12877_1_S1_L001_R2_001.fastq.gz'], data_datadir, reference, 'bwa', 'samtools')
+    data_datadir = "/Users/nuin/Projects/Data/Test_dataset"
+    sample_id = "NA12877_1"
+    reference = "/opt/reference/hg19.fasta"
+    run_bwa(
+        sample_id,
+        [
+            "/Users/nuin/Projects/Data/Test_dataset/BaseCalls/NA12877_1_S1_L001_R1_001.fastq.gz",
+            "/Users/nuin/Projects/Data/Test_dataset/BaseCalls/NA12877_1_S1_L001_R2_001.fastq.gz",
+        ],
+        data_datadir,
+        reference,
+        "bwa",
+        "samtools",
+    )
