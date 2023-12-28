6472af68f3f48e93771b5e00fa2ba04480a27aaa
Paulo Nuin <nuin@genedrift.org>
2023-12-28 10:44:26 -0700

    adding log entries - bwa

--- src/lib/bwa_align.py
+++ src/lib/bwa_align.py
@@ -14,6 +14,9 @@ from rich.console import Console
 
 from lib.utils import move_bam
 
+from pathlib import Path
+
+
 console = Console()
 
 logging.basicConfig(level=logging.INFO)
@@ -34,17 +37,38 @@ def run_bwa(sample_id, fastq_files, datadir, reference, bwa, samtools):
     """
 
     console.log(f"Starting bwa processing for file {sample_id}")
+    log_to_api(
+        f"Starting bwa processing for file {sample_id}",
+        "INFO",
+        "bwa_align",
+        sample_id,
+        Path(datadir).name,
+    )
 
     bam_index_check = 0
     if os.path.isfile(
         datadir + "/BAM/" + sample_id + "/" + sample_id + ".bam"
     ) or os.path.isfile(datadir + "/BAM/" + sample_id + "/BAM/" + sample_id + ".bam"):
         console.log(f"{sample_id} BAM file exists")
+        log_to_api(
+            f"{sample_id} BAM file exists",
+            "INFO",
+            "bwa_align",
+            sample_id,
+            Path(datadir).name,
+        )
         bam_index_check += 1
     elif os.path.isfile(
         datadir + "/BAM/" + sample_id + "/" + sample_id + ".bam.gz"
     ) or os.path.isfile(datadir + "/BAM/" + sample_id + "/BAM/" + sample_id + ".bam"):
         console.log(f"{sample_id} BAM file exists and compressed")
+        log_to_api(
+            f"{sample_id} BAM file exists and compressed",
+            "INFO",
+            "bwa_align",
+            sample_id,
+            Path(datadir).name,
+        )
         bam_index_check += 1
     else:
         bam_header = f"@RG\\tID:{sample_id}\\tLB:{datadir}\\tPL:Illumina\\tSM:{sample_id}\\tPU:None"
@@ -53,6 +77,13 @@ def run_bwa(sample_id, fastq_files, datadir, reference, bwa, samtools):
             f"| {samtools} view -Sb - > {datadir}/BAM/{sample_id}/BAM/{sample_id}.bam"
         )
         console.log(f"{bwa_string}")
+        log_to_api(
+            f"{bwa_string}",
+            "INFO",
+            "bwa_align",
+            sample_id,
+            Path(datadir).name,
+        )
         proc = subprocess.Popen(
             bwa_string, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE
         )
@@ -69,7 +100,21 @@ def run_bwa(sample_id, fastq_files, datadir, reference, bwa, samtools):
             f"{datadir}/BAM/{sample_id}/BAM/{sample_id}.bam"
         )
         console.log(f"{samtools_string}")
+        log_to_api(
+            f"{samtools_string}",
+            "INFO",
+            "bwa_align",
+            sample_id,
+            Path(datadir).name,
+        )
         console.log(f"Sorting BAM file {sample_id}")
+        log_to_api(
+            f"Sorting BAM file {sample_id}",
+            "INFO",
+            "bwa_align",
+            sample_id,
+            Path(datadir).name,
+        )
         proc = subprocess.Popen(
             samtools_string, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True
         )
@@ -84,6 +129,13 @@ def run_bwa(sample_id, fastq_files, datadir, reference, bwa, samtools):
         move_bam(datadir, sample_id, "sorted")
 
         console.log(f"Indexing BAM file {sample_id}")
+        log_to_api(
+            f"Indexing BAM file {sample_id}",
+            "INFO",
+            "bwa_align",
+            sample_id,
+            Path(datadir).name,
+        )
         samtools_string_2 = (
             f"{samtools} index {datadir}/BAM/{sample_id}/BAM/{sample_id}.bam"
         )
@@ -97,6 +149,13 @@ def run_bwa(sample_id, fastq_files, datadir, reference, bwa, samtools):
 
         bam_index_check = 0
         console.log(f"{sample_id} BAM file created")
+        log_to_api(
+            f"{sample_id} BAM file created",
+            "INFO",
+            "bwa_align",
+            sample_id,
+            Path(datadir).name,
+        )
 
     return bam_index_check
 
